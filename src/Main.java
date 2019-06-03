a0e2c14b9f6dfdf6bfff2fc6f4ec1dc2abd09a8c
Katie Cornish <katielcornish@gmail.com>
2019-06-03 14:09:26 +0100

    makes http request every 5 seconds

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        URL helloServer = new URL("http://localhost:5050/jedd");
-        URLConnection yc = helloServer.openConnection();
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(
-                        yc.getInputStream()));
-        String inputLine;
+        Timer t = new Timer();
+        t.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    newRequest();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 0, 5000);
+    }
 
-        while ((inputLine = in.readLine()) != null)
-            System.out.println(inputLine);
-        in.close();
+public static void newRequest() throws IOException {
+    URL helloServer = new URL("http://localhost:5050/jedd");
+    URLConnection helloConnection = helloServer.openConnection();
+    BufferedReader in = new BufferedReader(
+            new InputStreamReader(
+                    helloConnection.getInputStream()));
+    String inputLine;
+
+    while ((inputLine = in.readLine()) != null)
+        System.out.println(inputLine);
+    in.close();
     }
 }
+
