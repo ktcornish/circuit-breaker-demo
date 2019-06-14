c325c362f536e6c9b55d1d8ac2252914ae79c0f8
Jedd Hopkins <jedd.hopkins@capgemini.com>
2019-06-14 11:12:27 +0100

    Timer & Reader implemented

--- src/main/java/com/themis/circuitbreakerdemo/Main.java
+++ src/main/java/com/themis/circuitbreakerdemo/Main.java
@@ -3,6 +3,7 @@ package com.themis.circuitbreakerdemo;
 import net.jodah.failsafe.CircuitBreaker;
 import net.jodah.failsafe.Failsafe;
 import net.jodah.failsafe.RetryPolicy;
+import net.jodah.failsafe.function.CheckedRunnable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,21 +15,26 @@ import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.stream.Stream;
 
 public class Main extends TimerTask {
 
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
+    private static RetryPolicy<Object> retryPolicy;
+
+    private static CircuitBreaker<Object> circuitBreaker;
+
     public static void main(String[] args) {
 
-        RetryPolicy<Object> retryPolicy = new RetryPolicy<>()
+        retryPolicy = new RetryPolicy<>()
                 .withBackoff(1L, 10L, ChronoUnit.SECONDS)
                 .onRetriesExceeded(eae -> logger.debug("Retries exceeded {}", eae))
                 .onFailedAttempt(eae -> logger.debug("Failed attempt {}", eae))
                 .onRetry(eae -> logger.debug("Retry {}", eae))
                 .withMaxRetries(10);
 
-        CircuitBreaker<Object> circuitBreaker = new CircuitBreaker<>()
+        circuitBreaker = new CircuitBreaker<>()
                 .withFailureThreshold(3)
                 .withSuccessThreshold(3)
                 .withDelay(Duration.ofSeconds(5))
@@ -36,39 +42,30 @@ public class Main extends TimerTask {
                 .onOpen(() -> logger.debug("Open"))
                 .onHalfOpen(() -> logger.debug("Half Open"));
 
-        Failsafe
-                .with(retryPolicy, circuitBreaker)
-                .run(Main::startRequests);
-
-    }
-
-    private static void startRequests() throws IOException {
         TimerTask tasknew = new Main();
+
         Timer timer = new Timer();
-
         timer.scheduleAtFixedRate(tasknew, 0, 1000);
-
     }
 
     @Override
     public void run() {
-        try {
-            newRequest();
-        } catch (IOException e){
-            e.printStackTrace();
-        }
+
+        CheckedRunnable checkedRunnable = Main::newRequest;
+
+        Failsafe.with(retryPolicy, circuitBreaker)
+                .run(checkedRunnable);
     }
 
     private static void newRequest() throws IOException {
         URL helloServer = new URL("http://localhost:5050/");
 
-        try (InputStreamReader isr = new InputStreamReader(helloServer.openConnection().getInputStream());
-             BufferedReader br = new BufferedReader(isr)) {
+        InputStreamReader isr = new InputStreamReader(helloServer.openConnection().getInputStream());
+        BufferedReader br = new BufferedReader(isr);
 
-                String inputLine;
-                while ((inputLine = br.readLine()) != null)
-                    logger.debug(inputLine);
-             }
+        Stream<String> inputLine = br.lines();
+        inputLine.forEach(s -> logger.debug(s));
+
     }
 
 }
