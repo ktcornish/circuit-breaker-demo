bb8ce0c5934559c2dd14acc72d70b261b6fd4005
Jedd Hopkins <jedd.hopkins@capgemini.com>
2019-06-18 17:00:34 +0100

    Config class attributes declared final static

--- src/main/java/com/themis/circuitbreakerdemo/Config.java
+++ src/main/java/com/themis/circuitbreakerdemo/Config.java
@@ -2,12 +2,12 @@ package com.themis.circuitbreakerdemo;
 
 public class Config {
 
-    private int retryPolicyBackoffDelay = 1;
-    private int retryPolicyBackoffMaxDelay = 10;
-    private int retryPolicyMaxRetries = 10;
-    private int circuitBreakerFailureThreshold = 3;
-    private int circuitBreakerSuccessThreshold = 3;
-    private int circuitBreakerDelayDuration = 5;
+    private static final int retryPolicyBackoffDelay = 1;
+    private static final int retryPolicyBackoffMaxDelay = 10;
+    private static final int retryPolicyMaxRetries = 10;
+    private static final int circuitBreakerFailureThreshold = 3;
+    private static final int circuitBreakerSuccessThreshold = 3;
+    private static final int circuitBreakerDelayDuration = 5;
 
     public Config() {
     }
